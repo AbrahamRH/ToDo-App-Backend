2cd89f28a036f1391baf9acf1033ec4bc0d02adb
AbrahamRH <abrahamrzhz@gmail.com>
2023-02-22 09:43:25 -0600

    Iniciando projector con spring initializr

---
+++ src/test/java/com/todolist/Backendproject/BackEndProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todolist.Backendproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
