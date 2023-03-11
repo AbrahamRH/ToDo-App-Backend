2abed885c060e975ffabbf86b620de56b813c5d2
AbrahamRH <abrahamrzhz@gmail.com>
2023-03-10 18:41:00 -0600

    adding the delete mapping

--- src/main/java/com/todolist/Backendproject/Controller/TodoController.java
+++ src/main/java/com/todolist/Backendproject/Controller/TodoController.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -78,6 +79,15 @@ public class TodoController {
     service.findById(id).setDone(false);
     service.findById(id).setDoneDate(null);
     return ResponseEntity.ok().build();
+  }
+
+  @DeleteMapping (value="/todos/{id}")
+  public ResponseEntity<Void> deleteTodo(@PathVariable long id) {
+    if (!service.delete(id)) {
+      return ResponseEntity.notFound().build();
+    } else {
+      return ResponseEntity.ok().build();
+    }
   }
 
 }
